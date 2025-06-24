d404663c0830ec78e1e233b0834aed024cfd951c
sheikjalinrabei <abigail.eguia@escola.pr.gov.br>
2025-06-24 13:19:17 +0000

    Finalizando o meu projeto.

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Filme: I Saw the TV Glow',
+    'Quem dirigiu o filme "I Saw the TV Glow"?',
+    'Jane Schoenbrun.'
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
+
+criaCartao(
+    categoria,
+    pergunta,
+    resposta
+)
